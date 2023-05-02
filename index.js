76b3185cbab14c947b5baa773780575a15954a1e
Nicholas Lee <nicholasleexyz@gmail.com>
2023-05-02 16:15:26 -0600

    tileset files

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
 class Example extends Phaser.Scene {
     constructor() {
-        super();
+        super("hello-world");
     }
 
     preload() {
-        this.load.setBaseURL('https://labs.phaser.io');
+        // load the PNG file
+        this.load.image('DarkGrass', 'assets/DarkGrass.png');
 
-        this.load.image('sky', 'assets/skies/space3.png');
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-        this.load.image('red', 'assets/particles/red.png');
+        // load the JSON file
+        this.load.tilemapTiledJSON('tilemap', 'assets/DarkGrass.json');
+
+        // this.load.setBaseURL('https://labs.phaser.io');
+
+        // this.load.image('sky', 'assets/skies/space3.png');
+        // this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+        // this.load.image('red', 'assets/particles/red.png');
     }
 
     create() {
-        this.add.image(400, 300, 'sky');
+        // 👌 sanity check by displaying the entire tileset image
+        this.add.image(100, 100, 'DarkGrass')
 
-        const particles = this.add.particles(0, 0, 'red', {
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        });
+        // this.add.image(400, 300, 'sky');
 
-        const logo = this.physics.add.image(400, 100, 'logo');
+        // const particles = this.add.particles(0, 0, 'red', {
+        //     speed: 100,
+        //     scale: { start: 1, end: 0 },
+        //     blendMode: 'ADD'
+        // });
 
-        logo.setVelocity(100, 200);
-        logo.setBounce(1, 1);
-        logo.setCollideWorldBounds(true);
+        // const logo = this.physics.add.image(400, 100, 'logo');
 
-        particles.startFollow(logo);
+        // logo.setVelocity(100, 200);
+        // logo.setBounce(1, 1);
+        // logo.setCollideWorldBounds(true);
+
+        // particles.startFollow(logo);
     }
 }
 
